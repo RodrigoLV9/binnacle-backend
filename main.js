b4d76a9ebd808ce327a4e8913e1fba1fce3a6571
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-02-03 23:57:45 -0300

    feat(data): Add example data to have the database structure

--- main.js
+++ main.js
@@ -5,10 +5,14 @@ const PORT=process.env.PORT;
 const mongoose=require('mongoose')
 const password=process.env.PASSWORD;
 const authRoutes=require('./src/routes/auth.routes')
-mongoose.connect(`mongodb+srv://rodrigolv975:${password}@cluster0.rzn78.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,{})
+const exampleData=require('./src/data/example')
+/* mongoose.connect(`mongodb+srv://rodrigolv975:${password}@cluster0.rzn78.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,{}) */
 
 server.get('/api',(req,res)=>{
     res.send('Hello World');
+})
+server.get('/api/data',(req,res)=>{
+    res.send(exampleData)
 })
 server.use('/api',authRoutes)
 server.listen(PORT,()=>{
---
+++ src/data/example.js
@@ -0,0 +1,74 @@
+const users=[
+    {
+        'user_id':1,
+        'user_name':'RodrigoLV9',
+        'email':'rodrigolv.975@gmail.com',
+        'password':'12345',
+        'task':[
+            {
+                'id_task':1,
+                'date':'13/2',
+                'description':'dskfk dfsdsf sfdds sad s adas s asafae df wfwefgrwqaas'
+            },
+            {
+                'id_task':2,
+                'date':'14/2',
+                'description':'dskfk dfsdsf sfdds sad s adas s asafae df wfwefgrwqaas'
+            },
+            {
+                'id_task':3,
+                'date':'15/2',
+                'description':'dskfk dfsdsf sfdds sad s adas s asafae df wfwefgrwqaas'
+            }
+        ]
+    },
+    {
+        'user_id':2,
+        'user_name':'Sofia',
+        'email':'sofia123.sl4@gmail.com',
+        'password':'54321',
+        'task':[
+            {
+                'id_task':1,
+                'date':'13/2',
+                'description':'dskfk dfsdsf sfdds sad s adas s asafae df wfwefgrwqaas'
+            },
+            {
+                'id_task':2,
+                'date':'14/2',
+                'description':'dskfk dfsdsf sfdds sad s adas s asafae df wfwefgrwqaas'
+            },
+            {
+                'id_task':3,
+                'date':'15/2',
+                'description':'dskfk dfsdsf sfdds sad s adas s asafae df wfwefgrwqaas'
+            }
+        ]
+    },
+    {
+        'user_id':3,
+        'user_name':'Martin',
+        'email':'tinchorg.100@gmail.com',
+        'password':'contrasena',
+        'task':[
+            {
+                'id_task':1,
+                'date':'13/2',
+                'description':'dskfk dfsdsf sfdds sad s adas s asafae df wfwefgrwqaas'
+            },
+            {
+                'id_task':2,
+                'date':'14/2',
+                'description':'dskfk dfsdsf sfdds sad s adas s asafae df wfwefgrwqaas'
+            },
+            {
+                'id_task':3,
+                'date':'15/2',
+                'description':'dskfk dfsdsf sfdds sad s adas s asafae df wfwefgrwqaas'
+            }
+        ]
+    },
+
+]
+
+module.exports=users
