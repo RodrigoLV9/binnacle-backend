533da3767b9a738a34afc1f6b1385061fadcfed8
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-02-07 23:26:55 -0300

    feat(methods): Verify if username exist

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,20 +1,37 @@
-const User=require('../models/User')
-const register=async(req,res)=>{
-    const {username,email,password}=await req.body
-    const info_username=username
-    const info_email=email
-    const info_password=password
-    const task=[]
-    const newUser=new User({
-        user_name:info_username,
-        email:info_email,
-        password:info_password,
-        task:task
-    })
-    await newUser.save()
-    res.status(201).json({ message: "Usuario agregado con éxito", user: newUser });
-}
-const login=(req,res)=>{
-    res.send('Login')
-}
-module.exports={register,login}
+const User = require('../models/User');
+
+const register = async (req, res) => {
+    const { username, email, password } = req.body;
+    const task = [];
+
+    if (!username || !email || !password) {
+        res.status(400).json({
+            error: 'Faltan campos por rellenar'
+        });
+        return;
+    }
+
+    try {
+        const user=new User()
+        const exist=await user.userNameExist(username)
+        if(exist){
+            return res.status(400).json({error:'Username already exist'})
+        }
+        const newUser = new User({
+            user_name: username,
+            email: email,
+            password: password,
+            task: task
+        });
+        await newUser.save();
+        res.status(201).json({ message: "Usuario agregado con éxito", user: newUser });
+    } catch (error) {
+        res.status(500).json({ message: "Error al registrar el usuario", error: error.message });
+    }
+};
+
+const login = (req, res) => {
+    res.send('Login');
+};
+
+module.exports = { register, login };
--- src/models/User.js
+++ src/models/User.js
@@ -1,30 +1,38 @@
-const mongoose=require('mongoose')
-const bcrypt=require('bcrypt')
-const taskSchema=new mongoose.Schema({
-    id_task:String,
-    date:String,
-    description:String,
-})
-const UserSchema=new mongoose.Schema({
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const taskSchema = new mongoose.Schema({
+    id_task: String,
+    date: String,
+    description: String,
+});
+const UserSchema = new mongoose.Schema({
     user_name: String,
     email: String,
     password: String,
     task: [taskSchema],
-})
-UserSchema.pre('save',function(next){
-    if(this.isModified || this.isNew){
-        const document=this
-        bcrypt.hash(document.password,10,(err,hash)=>{
-            if(err){
-                next(err)
-            }else{
-                document.password=hash
-                next()
+});
+UserSchema.pre('save', function(next) {
+    if (this.isModified('password') || this.isNew) {
+        const document = this;
+        bcrypt.hash(document.password, 10, (err, hash) => {
+            if (err) {
+                next(err);
+            } else {
+                document.password = hash;
+                next();
             }
-        })
-    }else{
-        next()
+        });
+    } else {
+        next();
     }
-})
+});
+UserSchema.methods.userNameExist = async function(username) {
+    const result = await mongoose.model('User').findOne({ user_name: username });
+    return !!result;
+};
+UserSchema.methods.comparePassword = async function(password, hash) {
+    const same = await bcrypt.compare(password, hash);
+    return same;
+};
 const User = mongoose.model("User", UserSchema);
-module.exports=User
+module.exports = User;
