e59c92ab2048e50d2887330dcd89a2483f7e9600
RodrigoLV9 <rodrigolv.975@gmail.com>
2025-02-24 22:46:28 -0300

    feat(Binnacle.routes): Modify code 200 response of all https methods of binnacle routes

--- src/routes/binnacle.routes.js
+++ src/routes/binnacle.routes.js
@@ -2,17 +2,6 @@ const express=require('express')
 const binnacleRoutes=express.Router()
 const Binnacle=require('../models/Binnacle')
 const User=require('../models/User')
-const task=
-    [
-        {
-            description:'Programe 5hs en mi proyecto de binnacle',
-            date:'15/02/2025'
-        },
-        {
-            description:'Hice ejercicio 2hs y programe 6hs aparte de limpiar la casa',
-            date:'16/02/2025'
-        }
-    ]
 
 binnacleRoutes.get('/',async(req,res)=>{
     const {id}=req.query
@@ -25,17 +14,35 @@ binnacleRoutes.get('/',async(req,res)=>{
 })
 binnacleRoutes.post('/',async(req,res)=>{
     const {date,description,idUser}=req.body
-    
     if(date || description){
-        const newBinnacle=await new Binnacle({date:date,description:description,idUser:idUser}).save()
-        res.status(200).json({
-            date:newBinnacle.date,
-            description:newBinnacle.description,
-            idUser:newBinnacle.idUser
-        })
+        await new Binnacle({date:date,description:description,idUser:idUser}).save()
+        const binnacles=await Binnacle.find({idUser:idUser})
+        res.status(200).json(binnacles)
     }else{
         res.status(400).json({error:'Not date or description'})
     }
     
+})
+binnacleRoutes.put('/',async(req,res)=>{
+    const {id}=req.query
+    const {date,description,idUser}=req.body
+    if(id,date,description,idUser){
+        await Binnacle.findOneAndUpdate({_id:id},{date:date,description:description,idUser:idUser})
+        const binnacles=await Binnacle.find({idUser:idUser})
+        res.status(200).json(binnacles)
+    }else{
+        res.status(400).json({error:'Not id or date or description or idUser'})
+    }
+})
+binnacleRoutes.delete('/:iduser',async(req,res)=>{
+    const {iduser}=req.params
+    const {id}=req.query
+    if(id){
+        await Binnacle.findOneAndDelete({_id:id})
+        const binnacles=await Binnacle.find({idUser:iduser})
+        res.status(200).json(binnacles)
+    }else{
+        res.status(400).json({error:'Not id'})
+    }
 })
 module.exports=binnacleRoutes
